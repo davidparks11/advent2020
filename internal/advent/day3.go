adfe26e463e57af13ee61dd60d27c1943b9144f6
David Parks <dparks@us.ci.org>
2020-12-15 18:12:06 -0500

    complete day3 part2, revised part one

--- internal/advent/day3.go
+++ internal/advent/day3.go
@@ -8,29 +8,46 @@ type TobogganTrajectory struct {
 	dailyProblem
 }
 
+type slope struct {
+	x int
+	y int
+}
+
 func (t *TobogganTrajectory) Solve() {
 	t.day = 3
 	t.name = "Toboggan Trajectory"
 	input := t.GetInputLines()
 	var results []string
-	results = append(results, strconv.Itoa(t.treeCountInPath(input)))
+	results = append(results, strconv.Itoa(t.treeCountInPath(1, 3, input)))
 
+	part2Slopes := []slope{
+		{x: 1, y: 1},
+		{x: 3, y: 1},
+		{x: 5, y: 1},
+		{x: 7, y: 1},
+		{x: 1, y: 2},
+	}
+	part2Results := 1
+	for _, slope := range part2Slopes {
+		part2Results *= t.treeCountInPath(slope.y, slope.x, input)
+	}
+	results = append(results, strconv.Itoa(part2Results))
 	t.WriteResult(results)
 }
 
-func (t *TobogganTrajectory) treeCountInPath(trees []string) int {
+func (t *TobogganTrajectory) treeCountInPath(slopeY, slopeX int, treeLines []string) int {
 	treeCollisions := 0
 	const (
 		tree = uint8('#')
 	)
-	treePaternLength := len(trees[0])
-	horizontalTravel := 3
+	totalTreeLines := len(treeLines)
+	treePatternLength := len(treeLines[0])
 	horizontalIndex := 0
-	for _, treeLine := range trees {
-		if treeLine[horizontalIndex] == tree {
+	for i := 0; i < totalTreeLines; i += slopeY {
+		if treeLines[i][horizontalIndex] == tree {
 			treeCollisions++
 		}
-		horizontalIndex = (horizontalIndex+horizontalTravel)%treePaternLength
+		horizontalIndex = (horizontalIndex + slopeX) % treePatternLength
 	}
 	return treeCollisions
 }
