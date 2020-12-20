c5d00d115d1bdf70edd8f898d1ee97a08058a06d
David <davidparks11@gmail.com>
2020-12-20 16:21:50 -0500

    day 6 part 2 complete

--- internal/advent/day6.go
+++ internal/advent/day6.go
@@ -16,6 +16,7 @@ func (c *CustomCustoms) Solve() {
 	input := c.GetInputLines()
 	var results []string
 	results = append(results, strconv.Itoa(c.getCustomsYesCount(input)))
+	results = append(results, strconv.Itoa(c.getCustomsGroupYesCount(input)))
 	c.WriteResult(results)
 }
 
@@ -35,6 +36,34 @@ func (c *CustomCustoms) getCustomsYesCount(passengerAnswers []string) int {
 		if answerLine == "" || i == answerCount-1 {
 			yesCount += len(yesMap)
 			yesMap = make(map[rune]bool, 26)
+		}
+	} 
+	return yesCount
+}
+
+func (c *CustomCustoms) getCustomsGroupYesCount(passengerAnswers []string) int {
+	answerCount := len(passengerAnswers)
+	yesCount := 0 
+	groupTotal := 0
+	yesMap := make(map[rune]int, 26)
+	for i, answerLine := range passengerAnswers {
+
+		if answerLine != "" {
+			for _, answer := range answerLine {
+				if answer != ' ' {
+					yesMap[answer]++
+				}
+			}
+			groupTotal++
+		}
+		if answerLine == "" || i == answerCount-1 {
+			for _, answerCount := range yesMap {
+				if answerCount == groupTotal {
+					yesCount++
+				} 
+			}
+			yesMap = make(map[rune]int, 26)
+			groupTotal = 0
 		}
 	}
 	return yesCount
